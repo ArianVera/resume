a1afeaedb8388c488bde40adac39b266e6732909
Arian Vera <av92386@gmail.com>
2023-01-18 16:14:27 -0500

    Revert "updated portfolio"
    This reverts commit a2ed386139cc634571a1e1f7d529ef04bad95187.

--- js/extra.js
+++ js/extra.js
@@ -1,13 +1,4 @@
+
     const d = new Date();
     let year = d.getFullYear();
     document.getElementById("year").innerHTML = year;
-    $(document).ready(function(){
-        $("#slider").owlCarousel({
-            loop: true,
-            autoplay: true,
-            autoplayHoverPause: false,
-            autoplaySpeed: 100,
-            items: 1,
-            dots: false
-        });
-    });
